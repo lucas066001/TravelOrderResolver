b0987ded1799adfc8a5292a5b44fa1e85661abf8
lucas066001 <chapuis863@gmail.com>
2024-09-03 12:26:02 +0200

    fix(travel_resolver): Fix code formatting

--- App/travel_resolver/tests/sample_test.py
+++ App/travel_resolver/tests/sample_test.py
@@ -1,11 +1,12 @@
 import unittest
 from travel_resolver.libs.sample.greeter import GreeterHelper
 
+
 class TestGreeter(unittest.TestCase):
 
     def setUp(self):
         self._greeter = GreeterHelper()
 
     def test_correct_greeting(self):
-        sample_message = "François"
-        self.assertEqual(self._greeter.Greet(sample_message), "Hello ! François")
+        msg = "François"
+        self.assertEqual(self._greeter.Greet(msg), "Hello ! " + msg)
