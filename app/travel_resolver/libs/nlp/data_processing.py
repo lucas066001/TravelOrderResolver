62dade89325d9d6f2f7eb663d1517c10a60a9944
Az-r-ow <antoine.azar123@gmail.com>
2024-09-20 15:01:21 +0200

    refactor(data_processing): variable  name change + refactored method params

--- app/travel_resolver/libs/nlp/data_processing.py
+++ app/travel_resolver/libs/nlp/data_processing.py
@@ -27,7 +27,7 @@ def process_sentence(sentence: str, dep_token="<Dep>", arr_token="<Arr>") -> tup
     """
     Given a sentence, extract the departure and arrival locations and tokenize the sentence.
     Then assign labels to the tokens based on whether they are part of the departure or arrival locations.
-    Finally, return the logits and labels will be returned.
+    Finally, return the inputs and labels will be returned.
 
     Args:
       sentence (str): The sentence to process.
@@ -35,14 +35,14 @@ def process_sentence(sentence: str, dep_token="<Dep>", arr_token="<Arr>") -> tup
       arr_token (str): The token to mark the arrival location.
 
     Returns:
-      tuple: A tuple containing the logits and labels (logits, labels).
+      tuple: A tuple containing the inputs and labels (inputs, labels).
     """
     bare_sentence = sentence.replace(dep_token, "").replace(arr_token, "")
     departure = get_tagged_content(sentence, dep_token)
     arrival = get_tagged_content(sentence, arr_token)
     tokenized_sentence = nltk.word_tokenize(bare_sentence)
     labels = []
-    logits = []
+    inputs = []
     for token in tokenized_sentence:
         if token in departure:
             departure_labels = [2] * len(token)
@@ -54,54 +54,57 @@ def process_sentence(sentence: str, dep_token="<Dep>", arr_token="<Arr>") -> tup
             default_labels = [1] * len(token)
             labels.extend(default_labels)
         int_chars = [ord(char) for char in token]
-        logits.extend(int_chars)
+        inputs.extend(int_chars)
 
-    return (logits, labels)
+    return (inputs, labels)
 
 
 def convert_tagged_sentence_to_bio(
-    sentence: str, tags: list[str], entities: list[str]
+    sentence: str, tag_entities_pairs: list[tuple[str, str]]
 ) -> str:
     """
     Given a sentence with tags, convert the sentence to BIO format.
 
     Args:
       sentence (str): The sentence to convert to BIO format.
-      tags (list[str]): The tags to extract the entities from.
-      entities (list[str]): The entities to convert to BIO format.
+      tag_entities_pairs (list[tuple[str, str]]): The tags and entities to convert to BIO format
 
     Returns:
       str: The sentence in BIO format
 
     Example:
-      >>> convert_tagged_sentence_to_bio("Je voudrais voyager de <Dep>Nice<Dep> à <Arr>Clermont Ferrand<Arr>.", ["<Dep>", "<Arr>"], ["LOC-DEP", "LOC-ARR"])
+      >>> convert_tagged_sentence_to_bio("Je voudrais voyager de <Dep>Nice<Dep> à <Arr>Clermont Ferrand<Arr>.", [("Dep", "LOC-DEP"), ("Arr", "LOC-ARR")])
       Je O
       voudrais O
       voyager O
       de O
-      Nice LOC-DEP
+      Nice B-LOC-DEP
       à O
-      Clermont LOC-ARR
-      Ferrand LOC-ARR
+      Clermont B-LOC-ARR
+      Ferrand I-LOC-ARR
       . O
     """
     bare_sentence = sentence
+
+    tags = [pair[0] for pair in tag_entities_pairs]
+    entities = [pair[1] for pair in tag_entities_pairs]
+
+    for tag in tags:
+        bare_sentence = bare_sentence.replace(tag, "")
 
     # extended entities
     ext_entities = []
     for entity in entities:
         ext_entities.extend(["B-" + entity, "I-" + entity])
 
-    for tag in tags:
-        bare_sentence = bare_sentence.replace(tag, "")
-
-    for tag, entity in zip(tags, entities):
+    for tag, entity in tag_entities_pairs:
         while re.search(f"{tag}(.*?){tag}", sentence):
             match = re.search(f"{tag}(.*?){tag}", sentence)
             temp_entities = [entity] * len(nltk.word_tokenize(match.group(1)))
             temp_entities[0] = "B-" + entity
-            for i in range(1, len(temp_entities)):
-                temp_entities[i] = "I-" + entity
+            if len(temp_entities) > 1:
+                for i in range(1, len(temp_entities)):
+                    temp_entities[i] = "I-" + entity
             sentence = (
                 sentence[: match.start()]
                 + " ".join(temp_entities)
@@ -123,7 +126,7 @@ def convert_tagged_sentence_to_bio(
 
 
 def from_tagged_file_to_bio_file(
-    input_file: str, output_file: str, tags: list[str], entities: list[str]
+    input_file: str, output_file: str, tag_entities_pairs: list[tuple[str, str]]
 ) -> None:
     """
     Given an input file and an output file, read the input file, convert the content to BIO format, and write the content to the output file.
@@ -131,7 +134,7 @@ def from_tagged_file_to_bio_file(
     Args:
       input_file (str): The path to the input file.
       output_file (str): The path to the output file.
-      tags (list[str]): The tags to extract the entities from.
+      tag_entities_pairs (list[tuple[str, str]]): The tags and entities to convert to BIO format.
       entities (list[str]): The entities to convert to BIO format.
     """
     with open(input_file, "r") as file:
@@ -140,7 +143,7 @@ def from_tagged_file_to_bio_file(
     with open(output_file, "w") as file:
         sentences = content.split("\n")
         for sentence in tqdm(sentences):
-            bio_format = convert_tagged_sentence_to_bio(sentence, tags, entities)
+            bio_format = convert_tagged_sentence_to_bio(sentence, tag_entities_pairs)
             file.write(bio_format + "\n")
 
 
@@ -158,17 +161,18 @@ def from_bio_file_to_example(file_path: str) -> tuple:
 
     with open(file_path, "r") as file:
         content = file.read()
-    entities = content.split("\n")
+    lines = content.split("\n")
+
     logits = []
     labels = []
 
     unique_labels = set()
 
     # getting all the unique labels
-    for entity in entities:
-        if (len(entity.split(" "))) < 2:
+    for line in lines:
+        if (len(line.split(" "))) < 2:
             continue
-        word, label = entity.split(" ")
+        word, label = line.split(" ")
         unique_labels.add(label)
 
     unique_labels = list(unique_labels)
@@ -181,18 +185,18 @@ def from_bio_file_to_example(file_path: str) -> tuple:
 
     sentence_logits = []
     sentence_labels = []
-    for entity in entities:
-        if (len(entity.split(" "))) < 2:
+    for line in lines:
+        if (len(line.split(" "))) < 2:
             continue
-        word, label = entity.split(" ")
+        word, label = line.split(" ")
         ascii_code_chars = [ord(char) for char in word]
-        char_labels = [unique_labels[label]] * len(ascii_code_chars)
+        chars_labels = [unique_labels[label]] * len(ascii_code_chars)
         sentence_logits.extend(ascii_code_chars)
-        sentence_labels.extend(char_labels)
+        sentence_labels.extend(chars_labels)
         if word in stop_sentences:
             logits.append(sentence_logits)
             labels.append(sentence_labels)
             sentence_logits = []
             sentence_labels = []
 
-    return logits, labels
+    return [logits, labels]
