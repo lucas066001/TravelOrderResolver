08a86d884bb5edbc2314b38151b86cf09c05a1cc
Az-r-ow <antoine.azar123@gmail.com>
2024-10-31 16:52:52 +0100

    WIP(sentence_processing): notebook to show the difference between processing techniques

--- conv_tagged_file_to_bio.py
+++ conv_tagged_file_to_bio.py
@@ -1,8 +1,8 @@
 from app.travel_resolver.libs.nlp.data_processing import from_tagged_file_to_bio_file
 
 
-INPUT_FILE = "./data/scripting_lcs_1/sentences_10k.txt"
-OUTPUT_FILE = "./data/bio/fr.bio/fr.sentences.bio"
+INPUT_FILE = "./data/scripting_lcs_1/10k_samples.txt"
+OUTPUT_FILE = "./data/bio/fr.bio/10k_samples.bio"
 
 tag_entities_pairs = [("<Dep>", "LOC-DEP"), ("<Arr>", "LOC-ARR")]
 
