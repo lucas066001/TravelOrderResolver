a9302bd713fd0468800fb1e6916fba03ffe6d3d2
Az-r-ow <antoine.azar123@gmail.com>
2024-11-01 19:27:21 +0100

    feat: comparison with an unseen corpus

--- conv_tagged_file_to_bio.py
+++ conv_tagged_file_to_bio.py
@@ -1,8 +1,8 @@
 from app.travel_resolver.libs.nlp.data_processing import from_tagged_file_to_bio_file
 
 
-INPUT_FILE = "./data/scripting_lcs_1/10k_samples.txt"
-OUTPUT_FILE = "./data/bio/fr.bio/10k_samples.bio"
+INPUT_FILE = "./data/scripting_lcs_1/1k_new_samples.txt"
+OUTPUT_FILE = "./data/bio/fr.bio/1k_new_samples.bio"
 
 tag_entities_pairs = [("<Dep>", "LOC-DEP"), ("<Arr>", "LOC-ARR")]
 
